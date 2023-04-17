7c0cf02d49e394bec5d82a53e4c6c5b586ee0a66
rusted_iron_ingot <130846228+rusted-iron-ingot@users.noreply.github.com>
2023-04-17 22:54:52 +0800

    0.0.5-b3 dev updated 2023 4 17 22:54

--- src/mod/bettermite/base/Accessor.java
+++ src/mod/bettermite/base/Accessor.java
@@ -15,13 +15,13 @@ public class Accessor {
     public Accessor() {
     }
     
-    public static <T> T modify(@Nonnull Field field, @Nonnull T value) {
+    public static <T,Y> T modify(@Nonnull Field field, @Nonnull T value ,@Nonnull Y instance) {
         try {
             Field modifiers = Field.class.getDeclaredField("modifiers");
             modifiers.setAccessible(true);
             modifiers.setInt(field, field.getModifiers() & -17);
             field.setAccessible(true);
-            field.set(value, value);
+            field.set(instance, value);
             return (T) field.get(value);
         } catch (NoSuchFieldException e) {
             throw new RuntimeException(e);
@@ -34,12 +34,46 @@ public class Accessor {
         }
     }
     
-    public static <T,Y> Y access(@Nonnull Field field, @Nullable T instance) {
+    public static <T> T modifyStatic(@Nonnull Field field, @Nonnull T value) {
+        try {
+            Field modifiers = Field.class.getDeclaredField("modifiers");
+            modifiers.setAccessible(true);
+            modifiers.setInt(field, field.getModifiers() & -17);
+            field.setAccessible(true);
+            field.set(null, value);
+            return (T) field.get(value);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("Accessor：输入的值" + value + "无效", e);
+        } catch (ClassCastException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static <T,Y> Y access(@Nonnull Field field, @Nonnull T instance) {
         try {
             field.setAccessible(true);
             return (Y) field.get(instance);
         } catch (IllegalArgumentException e) {
             throw new RuntimeException("Accessor：你最好不是乱输的" + instance, e);
+        } catch (SecurityException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Accessor：快去告诉锈铁锭他Accessor有问题", e);
+        } catch (ClassCastException e){
+            throw new RuntimeException("Accessor：不可能，绝对不可能", e);
+        }
+    }
+    
+    public static <T> T accessStatic(@Nonnull Field field) {
+        try {
+            field.setAccessible(true);
+            return (T) field.get(null);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
         } catch (SecurityException e) {
             throw new RuntimeException(e);
         } catch (IllegalAccessException e) {
@@ -82,18 +116,7 @@ public class Accessor {
                 logger.error("Accessor：参数类型数量和参数数量不一致！");
                 return null;
             } else {
-                Class[] parameterTypes = new Class[args.length];
-                int i = 0;
-                Object[] var5 = args;
-                int var6 = args.length;
-                
-                for(int var7 = 0; var7 < var6; ++var7) {
-                    Object object = var5[var7];
-                    parameterTypes[i] = object.getClass();
-                    ++i;
-                }
-                
-                Constructor<T> constructor = clazz.getDeclaredConstructor(parameterTypes);
+                Constructor<T> constructor = clazz.getDeclaredConstructor(types);
                 constructor.setAccessible(true);
                 return constructor.newInstance(args);
             }
@@ -108,10 +131,23 @@ public class Accessor {
         }
     }
     
-    public static <T, Y> Y invoke(@Nonnull Method method, @Nullable T instance, @Nullable Object... args) {
+    public static <T, Y> Y invoke(@Nonnull Method method, @Nonnull T instance, @Nullable Object... args) {
         try {
             method.setAccessible(true);
             return (Y) method.invoke(instance, args);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NullPointerException e) {
+            throw new RuntimeException("Accessor：需要实例！", e);
+        }
+    }
+    
+    public static <T> T invokeStatic(@Nonnull Method method,@Nullable Object... args) {
+        try {
+            method.setAccessible(true);
+            return (T) method.invoke(null, args);
         } catch (InvocationTargetException e) {
             throw new RuntimeException(e);
         } catch (IllegalAccessException e) {
@@ -138,4 +174,5 @@ public class Accessor {
             throw new RuntimeException(e);
         }
     }
+    
 }
